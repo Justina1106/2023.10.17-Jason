060fd76cdef1a27442e72a1e915bdc4f5775d7fd
Justina1106 <jbycenkova@gmail.com>
2023-10-18 18:05:37 +0300

    Pridėta 2-tra užduotis

--- movies.js
+++ movies.js
@@ -19,3 +19,40 @@ async function displayRandomJoke() {
 
 const getJokeButton = document.getElementById('getJokeButton');
 getJokeButton.addEventListener('click', displayRandomJoke);
+
+async function chuckJokeCategories() {
+    const response = await fetch('https://api.chucknorris.io/jokes/categories')
+    const categories = await response.json()
+  
+    const categoriesSelect = document.querySelector('#category-select')
+  
+    categories.forEach(category => {
+      const categoryOption = document.createElement('option')
+      categoryOption.textContent = category
+  
+      categoriesSelect.append(categoryOption)
+    })
+  
+    const categoryForm = document.querySelector('#category-form')
+  
+    categoryForm.addEventListener('submit', async (event) => {
+      event.preventDefault()
+  
+      const selectedCategory = categoriesSelect.value
+      const categoryUrl = `https://api.chucknorris.io/jokes/random?category=` + selectedCategory
+  
+      // fetch(categoryUrl)
+      //   .then(response => response.json())
+      //   .then(joke => {
+      //     console.log(joke)
+      //   })
+  
+      const response = await fetch(categoryUrl)
+      const joke = await response.json()
+  
+      const jokeParagraph = document.querySelector('#joke-paragraph')
+      jokeParagraph.textContent = joke.value
+    })
+  }
+  
+  chuckJokeCategories()
